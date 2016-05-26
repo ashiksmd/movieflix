742b93c0ceb17bd5b3b8fb65292c07f8a372c5db
Ashik Samad <ashiksmd@gmail.com>
2016-05-25 20:30:56 -0700

    feat(module-ui) Enable pagination

--- js/browse.js
+++ js/browse.js
@@ -1,8 +1,59 @@
 var app = angular.module('catalog', []);
 app.controller('catalogCtrl', function($scope, $http) {
-    $http.get("data/movielist.json").then(function (response) {
-        movielist = response.data;
+    var pageSize = 12;
 
-        $scope.movielist = movielist.slice(0,12);
+    $scope.data = [];
+    $scope.page = 1;
+
+    // Enable/Disable Prev nav button
+    $scope.allowPrev = function() {
+        if ($scope.page > 1) {
+            return "";
+        }
+        return "disabled"
+    }
+
+    // Enable/Disable Next nav button
+    $scope.allowNext = function() {
+        var maxPages = Math.ceil($scope.data.length / pageSize);
+
+        if ($scope.page < maxPages) {
+            return "";
+        }
+        return "disabled"
+    }
+
+    // Display movies in current page
+    $scope.showPage = function() {
+        var movielist = $scope.data;
+        var total = movielist.length;
+        var page = $scope.page;
+        var start = (page-1)*pageSize;
+        var end = start + pageSize;
+
+        $scope.movielist = movielist.slice(start,end);
+    }
+
+    // Go to previous page
+    $scope.gotoPrev = function() {
+        if ($scope.page > 1) {
+            $scope.page--;
+            $scope.showPage();
+        }
+    }
+
+    // Go to next page
+    $scope.gotoNext = function() {
+        var maxPages = Math.ceil($scope.data.length / pageSize);
+        if ($scope.page < maxPages) {
+            $scope.page++;
+            $scope.showPage();
+        }
+    }
+
+    // Retrieve a list of movies to be displayed
+    $http.get("data/movielist.json").then(function (response) {
+        $scope.data = response.data;
+        $scope.showPage();
     });
 });
