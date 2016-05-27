9bf436ac6ee77e3c732255f622f0dc672cf91d29
Ashik Samad <ashiksmd@gmail.com>
2016-05-26 18:34:41 -0700

    feat(module-ui): add reviews page

---
+++ js/comments.js
@@ -0,0 +1,8 @@
+var app = angular.module('comments', ['ui.bootstrap']);
+app.controller('commentsCtrl', function($scope, $http) {
+    $scope.comments = [];
+
+    $http.get("data/comments.json").then(function (response) {
+        $scope.comments = response.data;
+    });
+});
